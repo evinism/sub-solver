776300a8b8b96dbd800fc6a2c7aa15382a7eda0c
Evin Sellin <evinism@gmail.com>
2015-04-19 01:57:00 -0700

    Added 3 more paragraphs to the plaintexts

--- plaintexts.js
+++ plaintexts.js
@@ -24,6 +24,22 @@ plaintext_list = [{
     plaintext: "Every inch of wall space is covered by a bookcase. Each bookcase has six shelves, going almost to the ceiling. Some bookshelves are stacked to the brim with hardback books: science, maths, history, and everything else. Other shelves have two layers of paperback science fiction, with the back layer of books propped up on old tissue boxes or lengths of wood, so that you can see the back layer of books above the books in front. And it still isn't enough. Books are overflowing onto the tables and the sofas and making little heaps under the windows.",
     author: "Eliezer Yudkowsky",
     origin: "excerpt from <b>Harry Potter and the Methods of Rationality</b>"
+},
+{
+    plaintext: "Medicine-as-commerce is at the heart of each of these stories just as it is at the heart of some of the good trends and most of the bad ones. It is clear enough that biotech and pharmaceutical firms can work miracles. But it is also true that they can lean heavily on public funding and end up making a great deal of private profit. Even more troublesome are the rapidly growing investor-owned health plans. They go under many names, including health-mantenance organizations. Although some of these are not-for-profit, many have in common a basic strategy: selling \"Product\" to \"Consumers\" rather than providing care to patients.",
+    author: "Paul Farmer",
+    origin: "excerpt from <b>Pathologies of Power</b>"
+},
+{
+    plaintext: "I imagined the Death Messenger as an ugly old man with horns and ulcerous skin, burning yellow eyes and a gaping, toothless mouth that waited to feed ravenously on the souls that lined up in front of our apartment. Our open door was Saja's gaping mouth, my mother his tongue, sampling each person for the taste of death. The demon waiting to snaatch me off to hell if I did not carry a red-packeted charm, Saja was the devil my father had preached about and, through my mother's chants and offerings, became more real to me than my father ever was.",
+    author: "Nora Okja Keller",
+    origin: "excerpt from <b>Comfort Woman</b>",
+    notes: "The J is likely difficult to figure out."
+},
+{
+    plaintext: "Mondays or Fridays, it doesn't matter, mornings always go by slow and this day especially. But lunchtime came finally and I got to get in line with the stay-at-school kids. Everything is fine until the nun who knows all the canteen kids by heart looks at me and says: You, who sent you here? And since I am shy, I don't say anything, just hold out my hand with the letter. This is no good, she says, till Sister Superior gives the okay. Go upstairs and see her. And so I went.",
+    author:"Sandra Cisneros",
+    origin:"excerpt from <b>The House on Mango Street</b>"
 }
 
 ]
